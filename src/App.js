95ffb3eb54267bfc7cb8913b0234537fcdd81c5a
Abdo <abdo55@gmail.com>
2024-10-10 19:50:32 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React</h1>
+      <h2>first App</h2>
+    </div>
+  );
+}
+
+export default App;
